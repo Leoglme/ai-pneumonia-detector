10d5f4ca2086e2afbbbacf544bca2d1a7382bb05
Corentin Recanzone <corentindevpro@hotmail.com>
2024-06-17 15:50:31 +0200

    feat: modify script create notebook add visualization

--- create_notebook.py
+++ create_notebook.py
@@ -17,29 +17,50 @@ n.cells.append(nbf.v4.new_markdown_cell('## Analyse des résultats des modèles 
 knn_results = load_results("knn_results.txt")
 cnn_results = load_results("cnn_results.txt")
 
-# Code pour afficher les résultats KNN
+# Ajouter les résultats KNN
+n.cells.append(nbf.v4.new_markdown_cell('### Résultats du modèle KNN'))
 knn_code = f"""
-print("Résultats KNN :\\n{knn_results}")
+knn_results = {knn_results}
+print("Précision KNN : ", knn_results['accuracy'])
+print("Score F1 KNN : ", knn_results['f1_score'])
+print("Rapport de classification KNN :\\n", knn_results['classification_report'])
 """
 n.cells.append(nbf.v4.new_code_cell(knn_code))
 
-# Code pour afficher les résultats CNN
+# Ajouter les résultats CNN
+n.cells.append(nbf.v4.new_markdown_cell('### Résultats du modèle CNN'))
 cnn_code = f"""
-print("Résultats CNN :\\n{cnn_results}")
+cnn_results = {cnn_results}
+print("Précision CNN : ", cnn_results['accuracy'])
+print("Score F1 CNN : ", cnn_results['f1_score'])
+print("Rapport de classification CNN :\\n", cnn_results['classification_report'])
 """
 n.cells.append(nbf.v4.new_code_cell(cnn_code))
 
 # Comparaison des modèles
+n.cells.append(nbf.v4.new_markdown_cell('### Comparaison des modèles'))
 comparison_code = """
-# Code pour générer des graphiques comparatifs
+import matplotlib.pyplot as plt
+
+# Données pour la comparaison
+labels = ['KNN', 'CNN']
+accuracy = [knn_results['accuracy'], cnn_results['accuracy']]
+f1_scores = [knn_results['f1_score'], cnn_results['f1_score']]
+
+# Création des graphiques
 fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-axs[0].bar(['KNN', 'CNN'], [knn_results['accuracy'], cnn_results['accuracy']])
+
+# Précision
+axs[0].bar(labels, accuracy, color=['blue', 'green'])
 axs[0].set_title('Comparaison de la précision')
 axs[0].set_ylabel('Précision')
 
-axs[1].bar(['KNN', 'CNN'], [knn_results['f1_score'], cnn_results['f1_score']])
+# Score F1
+axs[1].bar(labels, f1_scores, color=['blue', 'green'])
 axs[1].set_title('Comparaison du score F1')
 axs[1].set_ylabel('Score F1')
+
+plt.tight_layout()
 plt.show()
 """
 n.cells.append(nbf.v4.new_code_cell(comparison_code))
